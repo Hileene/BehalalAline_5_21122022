453f31f0953ada9cc96356161e997149d4eb2fe7
Hileene <linebeh@gmail.com>
2023-01-04 09:21:07 +0100

    Récupération de l'id de chaque produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,2 +1,11 @@
-const products = window.location.search;
-console.log (products);
+// RECUPERATION DE L'ID DE CHAQUE PRODUIT
+const idProduct = new URLSearchParams(window.location.search);
+const id = idProduct.get('id')
+console.log (id);
+
+const apiUrl = "http://localhost:3000/api/products/";
+
+fetch(`${apiUrl}/${id}`)
+    .then(response => response.json())
+    .then(product => console.log(product))
+    .catch(err => console.error(err));
