ae2a6b4aae98166293ca29e96459203bfb213e84
Hileene <linebeh@gmail.com>
2023-01-05 13:35:35 +0100

    Affichage img titre prix et description

--- front/js/product.js
+++ front/js/product.js
@@ -7,5 +7,47 @@ const apiUrl = "http://localhost:3000/api/products/";
 
 fetch(`${apiUrl}/${id}`)
     .then(response => response.json())
-    .then(product => console.log(product))
+    .then(product => displayProducts(product))
     .catch(err => console.error(err));
+
+//AFFICHAGE IMG, TITRE, PRIX, DESCRIPTION
+
+function displayProducts(product) {
+    const imgProduct = document.querySelector(".item__img")
+    const image = document.createElement("img")
+    image.src = product.imageUrl
+    image.alt = product.altTxt
+    imgProduct.appendChild(image);
+
+    const titleProduct = document.querySelector("#title")
+    const title = document.createElement("h1")
+    title.textContent = product.name
+    titleProduct.appendChild(title);
+
+    const priceProduct = document.querySelector("#price")
+    const price = document.createElement("p")
+    price.textContent = product.price
+    priceProduct.appendChild(price);
+
+    const descriptionProduct = document.querySelector("#description")
+    const description = document.createElement("p")
+    description.textContent = product.description
+    descriptionProduct.appendChild(description);
+
+    //AFFICHAGE COULEURS
+    const colorProduct = document.querySelector("#colors")
+    const color = document.createElement("option")
+    color.textContent = product.color
+    colorProduct.appendChild(color);
+
+   
+}
+
+
+
+
+
+
+
+
+
